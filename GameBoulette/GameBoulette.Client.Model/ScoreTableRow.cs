40f41ce6147873d495f88694f8fde233d1a065d6
MaxThom <maxthomassin@hotmail.com>
2021-08-04 09:43:29 +1200

    Added score screen !

---
+++ GameBoulette/GameBoulette.Client.Model/ScoreTableRow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoulette.Client.Model
+{
+    public class ScoreTableRow
+    {
+        public string Label { get; set; } = "";
+        public string WrittenBy { get; set; } = "";
+        public int TimesSkipped { get; set; }
+    }
+}
