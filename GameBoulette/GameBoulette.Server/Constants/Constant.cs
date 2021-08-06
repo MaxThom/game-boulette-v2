2b1bdae56fc7d0e64351041aa2b6fad868bc6d4c
MaxThom <maxthomassin@hotmail.com>
2021-08-06 14:26:44 +1200

    Added version fetching

---
+++ GameBoulette/GameBoulette.Server/Constants/Constant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameBoulette.Server.Constants
+{
+    public static class Constant
+    {
+        public static class Settings
+        {
+            public const string VersionKey = "VERSION_COMMIT";
+        }
+    }
+}
---
+++ GameBoulette/GameBoulette.Server/Controllers/VersionController.cs
@@ -0,0 +1,33 @@
+﻿using GameBoulette.Shared;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameBoulette.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+
+        private readonly ILogger<VersionController> _logger;
+
+        public VersionController(ILogger<VersionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var version = Environment.GetEnvironmentVariable(Constants.Constant.Settings.VersionKey);
+            _logger.LogInformation(version);
+            return version;
+        }
+    }
+}
